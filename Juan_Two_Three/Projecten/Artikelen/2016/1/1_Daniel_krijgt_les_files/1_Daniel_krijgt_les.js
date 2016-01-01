77b2e6bfb9423aded271a227f2c804e0d4b75d08
JonaMata <Jonathan@MekKlein.local>
2016-01-01 21:09:01 +0100

    Uploaded new website

---
+++ Juan_Two_Three/Projecten/Artikelen/2016/1/1_Daniel_krijgt_les_files/1_Daniel_krijgt_les.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20160101
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('1_Daniel_krijgt_les_files/1_Daniel_krijgt_lesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();IMpreload('1_Daniel_krijgt_les_files','shapeimage_3','0');IMpreload('1_Daniel_krijgt_les_files','shapeimage_4','0');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
